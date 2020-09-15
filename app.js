60af56d480b91d8bfdf7cd454e35bf0267ae6ff5
namans511 <srivastava.naman511@gmail.com>
2020-09-15 11:00:20 +0530

    first express server

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const http = require("http");
 const { Buffer } = require("buffer");
 
-const routes = require("./routes");
+const express = require("express");
+const app = express();
 
-const server = http.createServer(routes);
+app.use((req, res, next) => {
+  console.log("in the middleware!");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("in another middleware!");
+  res.send("<h1>whooo wo using express now</h1>");
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
